4bad8cff1ce1977267235e63f2a1031ffb9fdaff
mgaspari <marcoanthonygaspari@gmail.com>
2017-10-11 20:50:11 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
 // Code SimpleComponentHere Here
+import React from "react"
+
+export default class SimpleComponent extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      mood: "happy"
+    }
+  }
+  clickHandler = (e) => {
+    this.setState({
+      mood: "sad"
+    })
+  }
+  render(){
+    return(
+      <div onClick={this.clickHandler}>
+        {this.state.mood}
+      </div>
+    )
+  }
+}
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,18 @@
-// Code SimplerComponentHere Here
+// // Code SimplerComponentHere Here
+// import React from "react"
+//
+// class SimplerComponent extends React.Component{
+//   render(){
+//     return(
+//       <div class="simpler-component" onClick={this.props.handleClick}>I am just happy</div>
+//     )
+//   }
+// }
+//
+// export default SimplerComponent
+import React from "react"
+
+const SimplerComponent = function(props) {
+  return (<div class="simpler-component" onClick={this.props.handleClick}>I am just happy</div>)
+}
+export default SimplerComponent
